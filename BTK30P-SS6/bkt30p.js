d9f680eac07f5253f5f2c9c33d1ca0171c87d188
kienquyet1201 <kienquyet1201@gmail.com>
2026-02-04 13:32:35 +0700

    Add files via upload

---
+++ BTK30P-SS6/bkt30p.js
@@ -0,0 +1,34 @@
+let str = "Quý, Nam, Lan, Hùng, Nam";
+
+let students = str.split(",");
+console.log(students);
+
+students.reverse();
+console.log(students);
+
+if (students.includes("Lan")) {
+    console.log("Tên Lan có tồn tại trong mảng");
+} else {
+    console.log("Tên Lan không tồn tại trong mảng");
+}
+
+let indexNam = students.indexOf("Nam");
+console.log(indexNam);
+
+// let prices = [100, 200, 300, 400];
+
+// for (let price of prices) {
+//     console.log(price);
+// }
+
+// for (let index in prices) {
+//     console.log(index);
+// }
+
+// let total = 0;
+// for (let i = 0; i < prices.length; i++) {
+//     if (i % 2 === 0) {
+//         total += prices[i];
+//     }
+// }
+// console.log(total);
